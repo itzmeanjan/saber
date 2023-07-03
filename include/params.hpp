0b0bd0d7bbccf004ff71fcb7faaa5378d76ea1d6
Anjan Roy <hello@itzmeanjan.in>
2023-07-03 21:34:43 +0400

    check if unsigned integer value is power of 2 or not
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/params.hpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <type_traits>
+
+// Compile-time executable checks and parameters for Saber KEM implementation.
+namespace saber_params {
+
+// Given an unsigned integer, this routine can be used for checking whether
+// value of type T is a power of 2 or not, returning boolean truth value if
+// `val` is a power of 2.
+template<typename T>
+inline constexpr bool
+is_power_of_2(const T val)
+  requires(std::is_unsigned_v<T>)
+{
+  return !static_cast<bool>(val & (val - 1));
+}
+
+}
