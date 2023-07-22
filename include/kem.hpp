4246854f2795b963c4f99c44a8f65d6421ac1108
Anjan Roy <hello@itzmeanjan.in>
2023-07-22 11:52:55 +0400

    Saber KEM decapsulation algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/kem.hpp
+++ include/kem.hpp
@@ -118,4 +118,77 @@ encaps(std::span<const uint8_t, keyBytes> m, // step 1
   h256.reset();
 }
 
+// Given Saber KEM cipher text and Saber KEM secret key, this routine can be used for
+// decapsulating the received cipher text, extracting a shared secret key of 32 -bytes.
+// This is an implementation of algorithm 22 in section 8.5.3 of Saber spec.
+template<const size_t L,
+         const size_t EQ,
+         const size_t EP,
+         const size_t ET,
+         const size_t MU,
+         const size_t seedBytes,
+         const size_t keyBytes>
+inline void
+decaps(std::span<const uint8_t, saber_utils::kem_ctlen<L, EP, ET>()> ctxt,
+       std::span<const uint8_t,
+                 saber_utils::kem_sklen<L, EQ, EP, seedBytes, keyBytes>()> skey,
+       std::span<uint8_t, sha3_256::DIGEST_LEN> seskey)
+{
+  constexpr size_t pke_pklen = saber_utils::pke_pklen<L, EP, seedBytes>();
+  constexpr size_t pke_sklen = saber_utils::pke_sklen<L, EQ>();
+
+  // step 1
+  size_t off = 0;
+  auto z = skey.template subspan<off, keyBytes>();
+  off += keyBytes;
+  auto hash_pk = skey.template subspan<off, sha3_256::DIGEST_LEN>();
+  off += sha3_256::DIGEST_LEN;
+  auto pk = skey.template subspan<off, pke_pklen>();
+  off += pke_pklen;
+  auto sk = skey.template subspan<off, pke_sklen>();
+
+  std::array<uint8_t, sha3_256::DIGEST_LEN> m;
+  std::array<uint8_t, sha3_512::DIGEST_LEN> rk;
+  std::array<uint8_t, ctxt.size()> ctxt_prm;
+  std::array<uint8_t, sha3_256::DIGEST_LEN> r_prm;
+  std::array<uint8_t, keyBytes> temp;
+
+  // step 2
+  saber_pke::decrypt<L, EQ, EP, ET, MU>(ctxt, sk, m);
+
+  // step 3, 4
+  sha3_512::sha3_512 h512;
+  h512.absorb(m.data(), m.size());
+  h512.absorb(hash_pk.data(), hash_pk.size());
+  h512.finalize();
+  h512.digest(rk.data());
+  h512.reset();
+
+  // step 5
+  auto k = std::span(rk).subspan<0, keyBytes>();
+  auto r = std::span(rk).subspan<keyBytes, keyBytes>();
+
+  // step 6
+  saber_pke::encrypt<L, EQ, EP, ET, MU>(m, r, pk, ctxt_prm);
+
+  // step 7
+  auto c = saber_utils::ct_eq_bytes(ctxt_prm, ctxt);
+  // step 9, 10, 11, 12
+  saber_utils::ct_sel_bytes(c, temp, k, z);
+
+  // step 8
+  sha3_256::sha3_256 h256;
+  h256.absorb(ctxt.data(), ctxt.size());
+  h256.finalize();
+  h256.digest(r_prm.data());
+  h256.reset();
+
+  // step 13
+  h256.absorb(temp.data(), temp.size());
+  h256.absorb(r_prm.data(), r_prm.size());
+  h256.finalize();
+  h256.digest(seskey.data());
+  h256.reset();
+}
+
 }
