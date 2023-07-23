cbd656ba35da33e7108da3a551092b1348c9c38a
Anjan Roy <hello@itzmeanjan.in>
2023-07-23 08:16:45 +0400

    helper routine for benchmarking Saber encaps algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benchmarks/bench_kem.cpp
+++ benchmarks/bench_kem.cpp
@@ -1,5 +1,6 @@
 #include "kem.hpp"
 #include "prng.hpp"
+#include "sha3_256.hpp"
 #include <benchmark/benchmark.h>
 
 // Benchmark Saber KEM key generation algorithm for various suggested parameters.
@@ -42,6 +43,62 @@ keygen(benchmark::State& state)
     benchmark::DoNotOptimize(_z);
     benchmark::DoNotOptimize(_pkey);
     benchmark::DoNotOptimize(_skey);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(state.iterations());
+}
+
+// Benchmark Saber KEM encapsulation algorithm for various suggested parameters.
+template<const size_t L,
+         const size_t EQ,
+         const size_t EP,
+         const size_t ET,
+         const size_t MU,
+         const size_t seedBytes,
+         const size_t noiseBytes,
+         const size_t keyBytes>
+void
+encaps(benchmark::State& state)
+{
+  constexpr size_t pklen = saber_utils::kem_pklen<L, EP, seedBytes>();
+  constexpr size_t sklen = saber_utils::kem_sklen<L, EQ, EP, seedBytes, keyBytes>();
+  constexpr size_t ctlen = saber_utils::kem_ctlen<L, EP, ET>();
+
+  std::vector<uint8_t> seedA(seedBytes);
+  std::vector<uint8_t> seedS(noiseBytes);
+  std::vector<uint8_t> z(keyBytes);
+  std::vector<uint8_t> m(keyBytes);
+  std::vector<uint8_t> pkey(pklen);
+  std::vector<uint8_t> skey(sklen);
+  std::vector<uint8_t> ctxt(ctlen);
+  std::vector<uint8_t> seskey(sha3_256::DIGEST_LEN);
+
+  auto _seedA = std::span<uint8_t, seedBytes>(seedA);
+  auto _seedS = std::span<uint8_t, noiseBytes>(seedS);
+  auto _z = std::span<uint8_t, keyBytes>(z);
+  auto _m = std::span<uint8_t, keyBytes>(m);
+  auto _pkey = std::span<uint8_t, pklen>(pkey);
+  auto _skey = std::span<uint8_t, sklen>(skey);
+  auto _ctxt = std::span<uint8_t, ctlen>(ctxt);
+  auto _seskey = std::span<uint8_t, sha3_256::DIGEST_LEN>(seskey);
+
+  prng::prng_t prng;
+
+  prng.read(_seedA);
+  prng.read(_seedS);
+  prng.read(_z);
+  prng.read(_m);
+
+  saber_kem::keygen<L, EQ, EP, MU>(_seedA, _seedS, _z, _pkey, _skey);
+
+  for (auto _ : state) {
+    saber_kem::encaps<L, EQ, EP, ET, MU, seedBytes>(_m, _pkey, _ctxt, _seskey);
+
+    benchmark::DoNotOptimize(_m);
+    benchmark::DoNotOptimize(_pkey);
+    benchmark::DoNotOptimize(_ctxt);
+    benchmark::DoNotOptimize(_seskey);
     benchmark::ClobberMemory();
   }
 
