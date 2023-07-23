d417fa9c533012f1b39b73d9c936a0418d859137
Anjan Roy <hello@itzmeanjan.in>
2023-07-23 08:31:15 +0400

    register KEM routines of LightSaber, Saber and FireSaber parameter sets for benchmarking
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benchmarks/bench_kem.cpp
+++ benchmarks/bench_kem.cpp
@@ -24,20 +24,21 @@ keygen(benchmark::State& state)
   std::vector<uint8_t> pkey(pklen);
   std::vector<uint8_t> skey(sklen);
 
-  auto _seedA = std::span<uint8_t, seedBytes>(seedA);
-  auto _seedS = std::span<uint8_t, noiseBytes>(seedS);
-  auto _z = std::span<uint8_t, keyBytes>(z);
+  prng::prng_t prng;
+
+  prng.read(seedA);
+  prng.read(seedS);
+  prng.read(z);
+
+  auto _seedA = std::span<const uint8_t, seedBytes>(seedA);
+  auto _seedS = std::span<const uint8_t, noiseBytes>(seedS);
+  auto _z = std::span<const uint8_t, keyBytes>(z);
   auto _pkey = std::span<uint8_t, pklen>(pkey);
   auto _skey = std::span<uint8_t, sklen>(skey);
 
-  prng::prng_t prng;
-
-  prng.read(_seedA);
-  prng.read(_seedS);
-  prng.read(_z);
-
   for (auto _ : state) {
-    saber_kem::keygen<L, EQ, EP, MU>(_seedA, _seedS, _z, _pkey, _skey);
+    saber_kem::keygen<L, EQ, EP, MU, seedBytes, noiseBytes, keyBytes>(
+      _seedA, _seedS, _z, _pkey, _skey);
 
     benchmark::DoNotOptimize(_seedA);
     benchmark::DoNotOptimize(_seedS);
@@ -75,26 +76,27 @@ encaps(benchmark::State& state)
   std::vector<uint8_t> ctxt(ctlen);
   std::vector<uint8_t> seskey(sha3_256::DIGEST_LEN);
 
-  auto _seedA = std::span<uint8_t, seedBytes>(seedA);
-  auto _seedS = std::span<uint8_t, noiseBytes>(seedS);
-  auto _z = std::span<uint8_t, keyBytes>(z);
-  auto _m = std::span<uint8_t, keyBytes>(m);
+  prng::prng_t prng;
+
+  prng.read(seedA);
+  prng.read(seedS);
+  prng.read(z);
+  prng.read(m);
+
+  auto _seedA = std::span<const uint8_t, seedBytes>(seedA);
+  auto _seedS = std::span<const uint8_t, noiseBytes>(seedS);
+  auto _z = std::span<const uint8_t, keyBytes>(z);
+  auto _m = std::span<const uint8_t, keyBytes>(m);
   auto _pkey = std::span<uint8_t, pklen>(pkey);
   auto _skey = std::span<uint8_t, sklen>(skey);
   auto _ctxt = std::span<uint8_t, ctlen>(ctxt);
   auto _seskey = std::span<uint8_t, sha3_256::DIGEST_LEN>(seskey);
 
-  prng::prng_t prng;
-
-  prng.read(_seedA);
-  prng.read(_seedS);
-  prng.read(_z);
-  prng.read(_m);
-
   saber_kem::keygen<L, EQ, EP, MU>(_seedA, _seedS, _z, _pkey, _skey);
 
   for (auto _ : state) {
-    saber_kem::encaps<L, EQ, EP, ET, MU, seedBytes>(_m, _pkey, _ctxt, _seskey);
+    saber_kem::encaps<L, EQ, EP, ET, MU, seedBytes, keyBytes>(
+      _m, _pkey, _ctxt, _seskey);
 
     benchmark::DoNotOptimize(_m);
     benchmark::DoNotOptimize(_pkey);
@@ -132,25 +134,26 @@ decaps(benchmark::State& state)
   std::vector<uint8_t> seskey0(sha3_256::DIGEST_LEN);
   std::vector<uint8_t> seskey1(sha3_256::DIGEST_LEN);
 
-  auto _seedA = std::span<uint8_t, seedBytes>(seedA);
-  auto _seedS = std::span<uint8_t, noiseBytes>(seedS);
-  auto _z = std::span<uint8_t, keyBytes>(z);
-  auto _m = std::span<uint8_t, keyBytes>(m);
+  prng::prng_t prng;
+
+  prng.read(seedA);
+  prng.read(seedS);
+  prng.read(z);
+  prng.read(m);
+
+  auto _seedA = std::span<const uint8_t, seedBytes>(seedA);
+  auto _seedS = std::span<const uint8_t, noiseBytes>(seedS);
+  auto _z = std::span<const uint8_t, keyBytes>(z);
+  auto _m = std::span<const uint8_t, keyBytes>(m);
   auto _pkey = std::span<uint8_t, pklen>(pkey);
   auto _skey = std::span<uint8_t, sklen>(skey);
   auto _ctxt = std::span<uint8_t, ctlen>(ctxt);
   auto _seskey0 = std::span<uint8_t, sha3_256::DIGEST_LEN>(seskey0);
   auto _seskey1 = std::span<uint8_t, sha3_256::DIGEST_LEN>(seskey1);
 
-  prng::prng_t prng;
-
-  prng.read(_seedA);
-  prng.read(_seedS);
-  prng.read(_z);
-  prng.read(_m);
-
-  saber_kem::keygen<L, EQ, EP, MU>(_seedA, _seedS, _z, _pkey, _skey);
-  saber_kem::encaps<L, EQ, EP, ET, MU, seedBytes>(_m, _pkey, _ctxt, _seskey0);
+  saber_kem::keygen<L, EQ, EP, MU, seedBytes, noiseBytes, keyBytes>(
+    _seedA, _seedS, _z, _pkey, _skey);
+  saber_kem::encaps<L, EQ, EP, ET, MU, seedBytes, keyBytes>(_m, _pkey, _ctxt, _seskey0);
 
   for (auto _ : state) {
     saber_kem::decaps<L, EQ, EP, ET, MU, seedBytes, keyBytes>(_ctxt, _skey, _seskey1);
@@ -164,3 +167,16 @@ decaps(benchmark::State& state)
   assert(std::ranges::equal(_seskey0, _seskey1));
   state.SetItemsProcessed(state.iterations());
 }
+
+// Register for benchmarking LightSaber, Saber and FireSaber KEM routines.
+BENCHMARK(keygen<2, 13, 10, 10, 32, 32, 32>)->Name("lightsaber/keygen");
+BENCHMARK(encaps<2, 13, 10, 3, 10, 32, 32, 32>)->Name("lightsaber/encaps");
+BENCHMARK(decaps<2, 13, 10, 3, 10, 32, 32, 32>)->Name("lightsaber/decaps");
+
+BENCHMARK(keygen<3, 13, 10, 8, 32, 32, 32>)->Name("saber/keygen");
+BENCHMARK(encaps<3, 13, 10, 4, 8, 32, 32, 32>)->Name("saber/encaps");
+BENCHMARK(decaps<3, 13, 10, 4, 8, 32, 32, 32>)->Name("saber/decaps");
+
+BENCHMARK(keygen<4, 13, 10, 6, 32, 32, 32>)->Name("firesaber/keygen");
+BENCHMARK(encaps<4, 13, 10, 6, 6, 32, 32, 32>)->Name("firesaber/encaps");
+BENCHMARK(decaps<4, 13, 10, 6, 6, 32, 32, 32>)->Name("firesaber/decaps");
